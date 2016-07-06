d7c80aa3ac75b6513e24c7322b57e38340eb9fea
Jorin Vogel <hi@jorin.me>
2016-07-06 17:20:10 +0200

    output status in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
-# ssh setup
+echo ssh setup
 eval "$(ssh-agent -s)" # Start the ssh agent
 echo "$DEPLOY_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem # This key should have push access
 ssh-add deploy_key.pem
 
-# Get gh-pages branch
+echo Get gh-pages branch
 git clone -b gh-pages --single-branch git@github.com:trybash/game.git dist
-# Build into gh-pages branch
+echo Build into gh-pages branch
 npm run build
 
-# Commit changes with useful message
+echo Commit changes with useful message
 cd dist
 git add --all
 git commit -m "Automatic build for: '$(cd .. && git log -1 --pretty=%B)'"
