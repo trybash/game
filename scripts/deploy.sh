0bdcd723aced8e7b2624f49f4b4c2d3c9b878db9
Jorin Vogel <hi@jorin.me>
2016-07-06 16:28:27 +0200

    created deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,14 @@ echo "$DEPLOY_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem # This key should have push access
 ssh-add deploy_key.pem
 
+# Get gh-pages branch
+git clone -b gh-pages --single-branch git@github.com:trybash/game.git dist
+# Build into gh-pages branch
 npm run build
-git clone -b gh-pages --single-branch git@github.com:trybash/game.git
+
+# Commit changes with useful message
+cd dist
+git add --all
+git commit -m "Automatic build for: '$(cd .. && git log -1 --pretty=%B)'"
+
+git push origin gh-pages
