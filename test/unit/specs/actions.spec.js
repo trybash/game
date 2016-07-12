eeb965b6b17efe5932a4cac57eff8e1d18d7b7ab
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-07-12 22:35:27 +0200

    Group Tests

--- test/unit/specs/actions.spec.js
+++ test/unit/specs/actions.spec.js
@@ -1,28 +1,30 @@
 import * as actions from 'src/vuex/actions'
 
-describe('sendCommand', () => {
-  it('should dispatch command', () => {
-    const dispatch = sinon.spy()
-    actions.sendCommand({ dispatch }, 'Command')
-    expect(dispatch.calledOnce).to.be.true
-    expect(dispatch.calledWith('SEND_COMMAND', 'Command')).to.be.true
+describe('actions', () => {
+  describe('sendCommand', () => {
+    it('should dispatch command', () => {
+      const dispatch = sinon.spy()
+      actions.sendCommand({ dispatch }, 'Command')
+      expect(dispatch.calledOnce).to.be.true
+      expect(dispatch.calledWith('SEND_COMMAND', 'Command')).to.be.true
+    })
   })
-})
 
-describe('updateTask', () => {
-  it('should dispatch new task', () => {
-    const dispatch = sinon.spy()
-    actions.updateTask({ dispatch }, 'New Task')
-    expect(dispatch.calledOnce).to.be.true
-    expect(dispatch.calledWith('UPDATE_TASK', 'New Task')).to.be.true
+  describe('updateTask', () => {
+    it('should dispatch new task', () => {
+      const dispatch = sinon.spy()
+      actions.updateTask({ dispatch }, 'New Task')
+      expect(dispatch.calledOnce).to.be.true
+      expect(dispatch.calledWith('UPDATE_TASK', 'New Task')).to.be.true
+    })
   })
-})
 
-describe('reset', () => {
-  it('should dispatch reset', () => {
-    const dispatch = sinon.spy()
-    actions.reset({ dispatch })
-    expect(dispatch.calledOnce).to.be.true
-    expect(dispatch.calledWith('RESET')).to.be.true
+  describe('reset', () => {
+    it('should dispatch reset', () => {
+      const dispatch = sinon.spy()
+      actions.reset({ dispatch })
+      expect(dispatch.calledOnce).to.be.true
+      expect(dispatch.calledWith('RESET')).to.be.true
+    })
   })
 })
--- test/unit/specs/getters.spec.js
+++ test/unit/specs/getters.spec.js
@@ -14,75 +14,77 @@ const state = {
   completedLessons: [1, 2]
 }
 
-describe('getLessons', () => {
-  it('should return a list of title and index', () => {
-    const result = getters.getLessons(state)
-    expect(result).to.deep.equal([
-      {title: 'Title 0', index: 0},
-      {title: 'Title 1', index: 1}
-    ])
+describe('getters', () => {
+  describe('getLessons', () => {
+    it('should return a list of title and index', () => {
+      const result = getters.getLessons(state)
+      expect(result).to.deep.equal([
+        {title: 'Title 0', index: 0},
+        {title: 'Title 1', index: 1}
+      ])
+    })
   })
-})
 
-describe('getCompletedSectionCount', () => {
-  it('should return the current section - 1', () => {
-    const result = getters.getCompletedSectionCount(state)
-    expect(result).to.equal(1)
+  describe('getCompletedSectionCount', () => {
+    it('should return the current section - 1', () => {
+      const result = getters.getCompletedSectionCount(state)
+      expect(result).to.equal(1)
+    })
   })
-})
 
-describe('getSectionLength', () => {
-  it('should return length of current section', () => {
-    const result = getters.getSectionLength(state)
-    expect(result).to.equal(3)
+  describe('getSectionLength', () => {
+    it('should return length of current section', () => {
+      const result = getters.getSectionLength(state)
+      expect(result).to.equal(3)
+    })
   })
-})
 
-describe('getCompletedCount', () => {
-  it('should return how many lessons are completed', () => {
-    const result = getters.getCompletedCount(state)
-    expect(result).to.equal(2)
+  describe('getCompletedCount', () => {
+    it('should return how many lessons are completed', () => {
+      const result = getters.getCompletedCount(state)
+      expect(result).to.equal(2)
+    })
   })
-})
 
-describe('getLessonCount', () => {
-  it('should return how many lessons there are', () => {
-    const result = getters.getLessonCount(state)
-    expect(result).to.equal(2)
+  describe('getLessonCount', () => {
+    it('should return how many lessons there are', () => {
+      const result = getters.getLessonCount(state)
+      expect(result).to.equal(2)
+    })
   })
-})
 
-describe('getSection', () => {
-  it('should return the current section', () => {
-    const result = getters.getSection(state)
-    expect(result.task).to.equal('Section 2')
+  describe('getSection', () => {
+    it('should return the current section', () => {
+      const result = getters.getSection(state)
+      expect(result.task).to.equal('Section 2')
+    })
   })
-})
 
-describe('getTask', () => {
-  it('should return the current sections task', () => {
-    const result = getters.getTask(state)
-    expect(result).to.equal('Section 2')
+  describe('getTask', () => {
+    it('should return the current sections task', () => {
+      const result = getters.getTask(state)
+      expect(result).to.equal('Section 2')
+    })
   })
-})
 
-describe('getOutput', () => {
-  it('should return the output', () => {
-    const result = getters.getOutput(state)
-    expect(result).to.equal()
+  describe('getOutput', () => {
+    it('should return the output', () => {
+      const result = getters.getOutput(state)
+      expect(result).to.equal()
+    })
   })
-})
 
-describe('getHistory', () => {
-  it('should return the history', () => {
-    const result = getters.getHistory(state)
-    expect(result).to.equal()
+  describe('getHistory', () => {
+    it('should return the history', () => {
+      const result = getters.getHistory(state)
+      expect(result).to.equal()
+    })
   })
-})
 
-describe('getSolved', () => {
-  it('should return if the current section is solved', () => {
-    const result = getters.getSolved(state)
-    expect(result).to.equal(true)
+  describe('getSolved', () => {
+    it('should return if the current section is solved', () => {
+      const result = getters.getSolved(state)
+      expect(result).to.equal(true)
+    })
   })
 })
