6dd1b02cf8ef8578b72763348df2a665626ed5e8
Jorin Vogel <hi@jorin.me>
2016-07-12 15:21:22 +0200

    fixing deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,10 @@ git clone -b gh-pages --single-branch git@github.com:trybash/game.git dist
 echo Build into gh-pages branch
 npm run build
 
-echo "Commit changes with useful message"
+echo "Commit and push changes"
 cd dist
 git add --all
 git commit -m "Automatic build for: '$(cd .. && git log -1 --pretty=%B)'" \
-  || echo "Nothing new to commit" && exit
+  || (echo "Nothing new to commit" && exit)
 echo "Pushing updates"
 git push origin gh-pages
