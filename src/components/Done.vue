0b77d41541ac01f94a9263df83bcfc23e0ce87d3
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-07-28 00:47:08 +0200

    Add random congratulation in Done modal

--- src/components/Done.vue
+++ src/components/Done.vue
@@ -1,11 +1,17 @@
 <style lang="stylus">
-
+  .Done {
+    &-text {
+      margin-bottom 1rem
+      text-align center
+      font-size 2em
+    }
+  }
 </style>
 
 <template>
   <tb-modal :active="active">
     <div class="Done">
-      <div>Done!</div>
+      <div class="Done-text">{{praises[praiseIndex]}}</div>
       <tb-button @click="restartSection" color="red">Restart Section</tb-button>
       <tb-button @click="nextSection" v-el:next>Next Section →</tb-button>
     </div>
@@ -13,6 +19,7 @@
 </template>
 
 <script>
+  import _ from 'lodash'
   import Button from './Button'
   import Modal from './Modal'
   import { getDoneModalActive } from '../vuex/getters'
@@ -34,9 +41,30 @@
       }
     },
 
+    data () {
+      return {
+        praises: [
+          'Done!',
+          'Nice Work!',
+          'Wow!',
+          'Amazing!'
+        ],
+        praiseIndex: 0
+      }
+    },
+
     watch: {
       active (val) {
-        if (val) this.$els.next.focus()
+        if (val) {
+          this.$els.next.focus()
+          this.newPraise()
+        }
+      }
+    },
+
+    methods: {
+      newPraise () {
+        this.praiseIndex = _.random(this.praises.length - 1)
       }
     }
   }
--- test/unit/specs/Done.spec.js
+++ test/unit/specs/Done.spec.js
@@ -1,18 +1,49 @@
 import Done from 'src/components/Done'
 
 describe('Done.vue', () => {
+  describe('data', () => {
+    it('returns the initial data', () => {
+      const result = Done.data()
+      expect(result.praises).to.be.an.array
+      expect(result.praises.length).to.be.above(0)
+      expect(result.praiseIndex).to.equal(0)
+    })
+  })
+
   describe('watch', () => {
     describe('active', () => {
       it('calls focus() on $els.next if active became true', () => {
-        const spy = sinon.spy()
-        Done.watch.active.call({$els: {next: {focus: spy}}}, true)
-        expect(spy.calledOnce).to.be.true
+        const focus = sinon.spy()
+        const newPraise = sinon.spy()
+        Done.watch.active.call({$els: {next: {focus}}, newPraise}, true)
+        expect(focus.calledOnce).to.be.true
+        expect(newPraise.calledOnce).to.be.true
       })
 
       it('does not call focus() on $els.next if active became false', () => {
-        const spy = sinon.spy()
-        Done.watch.active.call({$els: {next: {focus: spy}}}, false)
-        expect(spy.calledOnce).to.be.false
+        const focus = sinon.spy()
+        const newPraise = sinon.spy()
+        Done.watch.active.call({$els: {next: {focus}}, newPraise}, false)
+        expect(focus.calledOnce).to.be.false
+        expect(newPraise.calledOnce).to.be.false
+      })
+    })
+  })
+
+  describe('methods', () => {
+    describe('newPraise', () => {
+      it('sets praiseIndex to some index of the praises array', () => {
+        for (var i = 0; i < 100; i++) {
+          const _this = {
+            praiseIndex: 0,
+            praises: ['a', 'b', 'c']
+          }
+
+          Done.methods.newPraise.call(_this)
+
+          expect(_this.praiseIndex).to.be.at.least(0)
+          expect(_this.praiseIndex).to.be.at.most(2)
+        }
       })
     })
   })
