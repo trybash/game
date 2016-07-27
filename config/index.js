e5a7fa9c7179646e237921939beee7c5b8d66c31
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-07-27 18:41:47 +0200

    Make Love Work

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/game/',
     productionSourceMap: true,
     // gzip off by default as many popular static hosts such as
     // surge or netlify already gzip all static assets for you
