be052873e6425a68bf5898231177cff3df83a951
Jorin Vogel <hi@jorin.me>
2016-07-28 17:50:00 +0200

    note on lastCommand function

--- src/lessons/utils.js
+++ src/lessons/utils.js
@@ -1,3 +1,4 @@
+// NOTE: current implementation does not support multiple sections with identical commands as solutions
 export function lastCommand (command) {
   return function (state) {
     return state.emulator.history[state.emulator.history.length - 1] === command
